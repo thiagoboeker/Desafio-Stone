867fbc6a9726ba5290ed36555282ceb506337202
thiagoboeker <thiago2kboeker@gmail.com>
2020-03-19 00:04:04 -0300

    entrypoint fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-while ! pg_isready -q -h $PG_HOST -p 5432 -U $PG_USER
-do
-  echo "$(date) Waiting POSTGRES to start"
-  sleep 2
-done
-
 ./prod/rel/stoned/bin/stoned eval Stoned.Release.migrate
 
 ./prod/rel/stoned/bin/stoned eval Stoned.Seeds.run
