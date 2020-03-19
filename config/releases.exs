de157c82ded686c0bd48438edfa56f8ebf0bd463
thiagoboeker <thiago2kboeker@gmail.com>
2020-03-18 23:27:27 -0300

    config for heroku

--- config/releases.exs
+++ config/releases.exs
@@ -1,3 +1,4 @@
+
 import Config
 
 secret_key_base = System.fetch_env!("SECRET_KEY_BASE")
@@ -6,17 +7,18 @@ hostname = System.fetch_env!("APP_HOSTNAME")
 pg_user = System.fetch_env!("PG_USER")
 pg_password = System.fetch_env!("PG_PASSWORD")
 pg_host = System.fetch_env!("PG_HOST")
+database_url = System.fetch_env!("DATABASE_URL")
 
 config :stoned, StonedWeb.Endpoint,
   http: [:inet6, port: String.to_integer(port)],
-  secret_key_base: secret_key_base
+  url: [scheme: "https", host: "hostname", port: 443]
+  secret_key_base: secret_key_base,
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 config :stoned, :app_port, port
 config :stoned, :app_hostname, hostname
 
 config :stoned, Stoned.Repo,
-  username: pg_user,
-  password: pg_password,
-  database: "stoned_prod",
-  hostname: pg_host,
+  ssl: true,
+  url: database_url,
   pool_size: 10
